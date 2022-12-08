ca4c6453804dce3093a5c06d2c3dc4191adf6c94
dangodswill99 <dangodswill99@gmail.com>
2022-12-08 14:36:44 -0800

    size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
+/**
 * main - Entry
 * Return: Always 0
 */
 int main(void)
 {
-	puts("Programming is like building a multi lingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multi lingual puzzle");
+return (0);
 }
+
