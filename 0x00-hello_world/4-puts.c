87665f3211d72d2f6cb5710b5c670f5b15a1ebbc
dangodswill99 <dangodswill99@gmail.com>
2022-12-08 14:54:05 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* main - Entry
-* Return: Always 0
-*/
+ *main - Receives no arguement value
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multi lingual puzzle");
-return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
-
