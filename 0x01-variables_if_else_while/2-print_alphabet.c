9ff2f66893f55b82469a300f97f5c5eb66816e03
dangodswill99 <dangodswill99@gmail.com>
2022-12-09 14:19:13 -0800

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * description: get a random number with 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= '2')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
